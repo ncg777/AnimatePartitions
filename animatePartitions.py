91bdbb043d85667cc25bb6d82f622f0902b82d39
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-08-21 02:04:39 -0400

    fix + new example

--- animatePartitions.py
+++ animatePartitions.py
@@ -127,6 +127,10 @@ def save_animation(partitions, filename):
             os.remove(file)
 
 
-if __name__ == "main"
-    partitions = [[2,1,1,0,1,2,2,3],[3,1,1,0,1,2,2,3],[3,1,1,0,1,3,2,3],[3,1,0,0,1,3,2,3],[1,1,0,0,1,3,2,3],[1,1,1,0,1,3,2,3],[1,1,1,0,1,2,2,3]]
-    save_animation(partitions, "animation.gif")
+if __name__ == '__main__':
+    partitions = [
+        [[2,1,1,0,1,2,2,3],[3,1,1,0,1,2,2,3],[3,1,1,0,1,3,2,3],[3,1,0,0,1,3,2,3],[1,1,0,0,1,3,2,3],[1,1,1,0,1,3,2,3],[1,1,1,0,1,2,2,3]],
+        [[0,0,1,2,2,3,3,4],[0,0,1,3,2,3,3,4],[0,0,1,3,2,2,3,4],[0,0,1,2,2,2,3,4]]
+    ]
+    save_animation(partitions[0], "animation1.gif")
+    save_animation(partitions[1], "animation2.gif")
